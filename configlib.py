475ba8be7d2597f89f2b61ba86ec2b79a5665915
Richard Neumann <r.neumann@homeinfo.de>
2018-12-07 11:23:35 +0100

    Refac.

--- configlib.py
+++ configlib.py
@@ -1,8 +1,9 @@
 """Configuration file parsing library."""
 
-from os.path import getmtime
 from configparser import ConfigParser
 from json import load
+from os.path import getmtime
+from pathlib import Path
 
 
 __all__ = ['parse_bool', 'INIParser', 'JSONParser']
@@ -25,7 +26,7 @@ def parse_bool(value):
     return BOOLEAN_STRINGS.get(value.strip().lower())
 
 
-class AlertParser:
+class AlertParserMixin:
     """Abstract configuration file parser that recognizes file changes."""
 
     def __init__(self, file, encoding=None, alert=False):
@@ -34,7 +35,7 @@ class AlertParser:
         Optionally the configuration can be set to be alert to
         file modification time changes to reload it accordingly.
         """
-        self.file = file
+        self.file = Path(file)
         self.encoding = encoding
         self.alert = alert
         self.mtime = None
@@ -58,20 +59,16 @@ class AlertParser:
                 the file's timestamp changed.
         """
         if force or not self.loaded or self.alert and self.changed:
-            self.read(self.file, encoding=self.encoding)
-            self.mtime = getmtime(self.file)
-
-    def read(self, filename, encoding=None):
-        """Reads the file."""
-        raise NotImplementedError()
+            self.read(str(self.file), encoding=self.encoding)
+            self.mtime = getmtime(str(self.file))
 
 
-class INIParser(ConfigParser, AlertParser):     # pylint: disable=R0901
+class INIParser(ConfigParser, AlertParserMixin):     # pylint: disable=R0901
     """Parses INI-ish configuration files."""
 
     def __init__(self, file, encoding=None, alert=False, **kwargs):
         """Invokes super constructors."""
-        AlertParser.__init__(self, file, encoding=encoding, alert=alert)
+        AlertParserMixin.__init__(self, file, encoding=encoding, alert=alert)
         ConfigParser.__init__(self, **kwargs)
 
     def __getitem__(self, item):
@@ -81,8 +78,13 @@ class INIParser(ConfigParser, AlertParser):     # pylint: disable=R0901
         self.load()
         return super().__getitem__(item)
 
+    def read(self, filename, encoding=None):    # pylint: disable=W0221
+        """Reads the file."""
+        self.file = Path(filename)
+        return super().read(filename, encoding=encoding)
 
-class JSONParser(AlertParser):
+
+class JSONParser(AlertParserMixin):
     """Parses JSON-ish configuration files."""
 
     def __init__(self, file, encoding=None, alert=False):
@@ -112,4 +114,5 @@ class JSONParser(AlertParser):
         except (FileNotFoundError, PermissionError, ValueError):
             return False
 
+        self.file = Path(filename)
         return True
